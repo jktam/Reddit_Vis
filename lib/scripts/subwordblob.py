6ab4de50bdd345d4e3a2fb5e878c8f6c7a4df42d
jktam <james.kit.tam@gmail.com>
2016-03-07 12:49:36 -0800

    edited subwordblob..py

--- lib/scripts/subwordblob.py
+++ lib/scripts/subwordblob.py
@@ -6,6 +6,7 @@ path = "C:/Users/James/Desktop/Reddit_Vis/lib/data2"
 dirs = os.listdir( path )
 dicts = []
 blob = ""
+
 for file in dirs:
     subredditName = file.split('.',1)[0]
     with open("C:/Users/James/Desktop/Reddit_Vis/lib/data2/"+file, 'r', encoding="iso-8859-1") as f:
@@ -17,6 +18,7 @@ for file in dirs:
         # print(row['title'])
         blob += row['title']+" "+row['selftext']
     dicts.append({subredditName: blob})
-# print(dicts)
-with open("C:/Users/James/Desktop/asdf.txt", 'w') as f:
-	json.dump(dicts,f)
+
+print("Just took a dump.")
+with open("C:/Users/James/Desktop/blobdump.json", 'w') as f:
+    json.dump(dicts,f,indent=4)
